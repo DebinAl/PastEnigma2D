9d3b94b82168d9eb2f622a20ef8058190fa0db4b
Debin <99970804+DebinAl@users.noreply.github.com>
2023-11-08 15:11:00 +0800

    First Init

---
+++ Assets/Script/PlayerScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
